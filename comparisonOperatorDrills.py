dae2b0a540a8fe500c5210b0a383dc79c858596b
EthanOyen <55327075+EthanOyen@users.noreply.github.com>
2019-10-05 11:50:43 -0500

    printed variables

--- comparisonOperatorDrills.py
+++ comparisonOperatorDrills.py
@@ -5,7 +5,6 @@ EXAMPLE TASK:
 '''
 #EX) Check if 4 is equal to 5 and store the answer in variable a
 a = (4==5)
-
 '''
 END OF EXAMPLE
 '''
@@ -16,50 +15,64 @@ START HERE
 
 #1) Check if "dog" is equal to "cat" and store the answer in variable b
 b = ("dog" == "cat")
+print("b: " + str(b))
 
 #2) Check if 1.24 is equal to 1.25 and store the answer in variable c
 c = (1.24 == 1.25)
+print("c: " + str(c))
 
 #3) Check if True is equal to False and store the answer in variable d
 d = (True == False)
+print("d: " + str(d))
 
 #4) Check if [1,2,3] is equal to [1,2,3] and store the answer in variable e
 e = ([1,2,3] == [1,2,3])
+print("e: " + str(e))
 
 #5) Check if 1 is less than or equal to 2 and store the answer in variable f
 f = (1 <= 2)
+print("f: " + str(f))
 
 #6) Check if 1 is greater than or equal to 2 and store the answer in variable g
 g = (1 >= 2)
+print("g: " + str(g))
 
 #7) Check if 1.66 is greater than or equal to 1.67 and store the answer in variable h
 h = (1.66 >= 1.67)
+print("h: " + str(h))
 
 #8) Check if 1.66 is less than or equal to 1.67 and store the answer in variable i
 i = (1.66 <= 1.67)
+print("i: " + str(i))
 
 #9) Check if 1 is less than 2 and store the answer in variable j
 j = (1 < 2)
+print("j: " + str(j))
 
 #10) Check if 1 is greater than 2 and store the answer in variable k
 k = (1 > 2)
+print("k: " + str(k))
 
 #11) Check if 11 divided by three 3 is less than 100 divided by 33 and store the answer
 #  ) in variable l
 l = (11/3 < 100/33)
+print("l: " + str(l))
 
 #12) Check if 9 times 7 is greater than 8 times 8 and store the answer in variable m
 m = (9*7 > 8*8)
+print("m: " + str(m))
 
 #13) Check if the square root of 3 is less than 1.15 squared and store the answer in
 #  ) variable n
 n = (math.sqrt(3) < 1.15**2)
+print("n: " + str(n))
 
 #14) Multiple 8 by 3 and store that in variable o, then divide 56 by 2 and store that
 #  ) in variable p, finally check if o is greater then p and store the answer in variable q.
 o = 8*3
 p = 56/2
 q = (o > p)
+print("q: " + str(q))
 
 #15) Take the square root of 96 and store that in variable r, then square 8.6 and store
 #  ) that in variable s, finally check if r is greater than or equal to s and store the
